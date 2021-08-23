ff432058923134efb3c92e7ad870752c5ad4a86f
Korbinian.Schweiger <korbinian.schweiger@hella-gutmann.com>
2021-08-23 17:14:01 +0200

    Updates to personal spacemacs configs

--- spacemacs_private/config.el
+++ spacemacs_private/config.el
@@ -1,8 +1,19 @@
+(delete-selection-mode 1)
+
 ;; Windows stuff
 (setq w32-pass-rwindow-to-system nil)
 (setq w32-rwindow-modifier 'super)
+(setq tramp-default-method "plink")
 
+;; TRAMP
+(add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+(add-to-list 'tramp-remote-process-environment (format "PYENV_VERSION=%s" "3.7.11"))
+(add-to-list 'tramp-remote-process-environment (format "PYENV_ROOT=%s" "$HOME/.pyenv"))
 
+;;(setq python-shell-interpreter "c:/Users/korbinian.schweiger/AppData/Local/Programs/Python/Python37/python.exe")
+
+;; Treemacs config
+(setq treemacs-python-executable "c:/Users/korbinian.schweiger/AppData/Local/Programs/Python/Python37/python.exe")
 (setq treemacs-no-png-images "")
 (setq treemacs-show-cursor "")
 
@@ -13,3 +24,14 @@
         (string-suffix-p "pdf" absolute-path)))
 
   (add-to-list 'treemacs-ignored-file-predicates #'treemacs-ignore-example))
+
+
+;; ORG
+(setq org-agenda-files
+      (list "c:/Users/korbinian.schweiger/Documents/Notes/hgs.org"
+	          "c:/Users/korbinian.schweiger/Documents/Notes/outlier_modes.org")
+      )
+
+;; Perspectives
+;;(add-hook 'kill-emacs-hook #'persp-state-save)
+(setq layouts-enable-autosave t)
