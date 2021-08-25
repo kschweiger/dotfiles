8cafcc675ffed39c36064b8bfbe741506e649303
Korbinian Schweiger <korbinian.schweiger@cern.ch>
2021-08-25 22:25:44 +0200

    updates

--- spacemacs_private/config.el
+++ spacemacs_private/config.el
@@ -1,14 +1,20 @@
 (delete-selection-mode 1)
 
 ;; Windows stuff
-(setq w32-pass-rwindow-to-system nil)
-(setq w32-rwindow-modifier 'super)
-(setq tramp-default-method "plink")
+;; (setq w32-pass-rwindow-to-system nil)
+;; (setq w32-rwindow-modifier 'super)
+;; (setq tramp-default-method "plink")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;; macOS Setting ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq ns-right-alternate-modifier nil)
+(setq mac-right-option-modifier nil)
+;;Make option work as meta (as it would be on Linux)
+(setq mac-option-modifier 'meta)
 
 ;; TRAMP
 ;;(add-to-list 'tramp-remote-path 'tramp-own-remote-path)
 ;;(add-to-list 'tramp-remote-process-environment (format "PYENV_VERSION=%s" "3.7.11"))
-;;(add-to-list 'tramp-remote-process-environment (format "PYENV_ROOT=%s" "$HOME/.pyenv"))
+;;(add-to-list 'tramp-remote-process-environmefeRnt (format "PYENV_ROOT=%s" "$HOME/.pyenv"))
 
 ;;(setq python-shell-interpreter "c:/Users/korbinian.schweiger/AppData/Local/Programs/Python/Python37/python.exe")
 
