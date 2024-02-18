7fe683758f9f7c0d7d7340c81067592ca59c75a5
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2024-02-18 11:54:55 +0100

    Started a nvim config with lazy

---
+++ nvim/lua/custom/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = { -- required(bufferline).setup(opts)
+    options = {
+      mode = "tabs",
+      separator_style = "slant",
+    },
+  },
+}
