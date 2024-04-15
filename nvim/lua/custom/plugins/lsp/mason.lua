d89f659e53f1d976e2511fad133a2020cee77826
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2024-04-15 13:02:58 +0200

    Minor changes to nvim config

--- nvim/lua/custom/plugins/lsp/mason.lua
+++ nvim/lua/custom/plugins/lsp/mason.lua
@@ -32,6 +32,7 @@ return {
         "cssls",
         "lua_ls",
         "pyright",
+        "ruff_lsp",
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -1,4 +1,4 @@
-lua/custom/plugins/telescope.luareturn {
+return {
   "nvim-telescope/telescope.nvim",
   branch = "0.1.x",
   dependencies = {
