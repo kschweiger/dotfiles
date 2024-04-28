632efb1884dd648df825c8ffff86c48134406555
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2024-04-28 15:08:47 +0200

    vim+tmux setup

--- nvim/lua/custom/core/keymaps.lua
+++ nvim/lua/custom/core/keymaps.lua
@@ -2,3 +2,9 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
+
+-- Pane Navigation
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
