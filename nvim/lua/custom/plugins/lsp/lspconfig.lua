6982471ba306aa84142099c1c7c2e7a37157cec5
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2024-05-07 08:53:50 +0200

    Fixing lsp keymap

--- nvim/lua/custom/plugins/lsp/lspconfig.lua
+++ nvim/lua/custom/plugins/lsp/lspconfig.lua
@@ -25,12 +25,12 @@ return {
         -- set keybinds
         opts.desc = "Show LSP references"
         --keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
-        keymap.set("n", "gr", vim.lsp.buf.references(), opts)
+        keymap.set("n", "gr", vim.lsp.buf.references, opts)
         opts.desc = "Go to declaration"
         keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
         opts.desc = "Show LSP definitions"
         --keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-        keymap.set("n", "gd", vim.lsp.buf.definition(), opts)
+        keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         opts.desc = "Show LSP implementations"
         keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
         opts.desc = "Show LSP type definitions"
