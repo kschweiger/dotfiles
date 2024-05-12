32f292fc9d3d92a895842ea42eeb1e5c76131a67
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2024-05-12 23:40:05 +0200

    [nvim] Updated harpoon config; Added treesitter-context

--- nvim/lua/custom/plugins/harpoon.lua
+++ nvim/lua/custom/plugins/harpoon.lua
@@ -1,60 +1,53 @@
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
   config = function()
-    require("harpoon"):setup()
+    local harpoon = require("harpoon")
+    harpoon:setup()
+    -- basic telescope configuration
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        })
+        :find()
+    end
+
+    vim.keymap.set("n", "<leader>a", function()
+      harpoon:list():add()
+    end, { desc = "harpoon file" })
+
+    vim.keymap.set("n", "<leader>A", function()
+      toggle_telescope(harpoon:list())
+    end, { desc = "Open harpoon window" })
+
+    vim.keymap.set("n", "<leader>1", function()
+      harpoon:list():select(1)
+    end, { desc = "harpoon to file 1" })
+    vim.keymap.set("n", "<leader>2", function()
+      harpoon:list():select(2)
+    end, { desc = "harpoon to file 2" })
+    vim.keymap.set("n", "<leader>3", function()
+      harpoon:list():select(3)
+    end, { desc = "harpoon to file 3" })
+    vim.keymap.set("n", "<leader>4", function()
+      harpoon:list():select(4)
+    end, { desc = "harpoon to file 4" })
+
+    vim.keymap.set("n", "<leader>fd", function()
+      harpoon:list():remove()
+    end, { desc = "Remove harpooned file" })
   end,
-  keys = {
-    {
-      "<leader>A",
-      function()
-        require("harpoon"):list():append()
-      end,
-      desc = "harpoon file",
-    },
-    {
-      "<leader>a",
-      function()
-        local harpoon = require("harpoon")
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end,
-      desc = "harpoon quick menu",
-    },
-    {
-      "<leader>1",
-      function()
-        require("harpoon"):list():select(1)
-      end,
-      desc = "harpoon to file 1",
-    },
-    {
-      "<leader>2",
-      function()
-        require("harpoon"):list():select(2)
-      end,
-      desc = "harpoon to file 2",
-    },
-    {
-      "<leader>3",
-      function()
-        require("harpoon"):list():select(3)
-      end,
-      desc = "harpoon to file 3",
-    },
-    {
-      "<leader>4",
-      function()
-        require("harpoon"):list():select(4)
-      end,
-      desc = "harpoon to file 4",
-    },
-    {
-      "<leader>5",
-      function()
-        require("harpoon"):list():select(5)
-      end,
-      desc = "harpoon to file 5",
-    },
-  },
 }
---
+++ nvim/lua/custom/plugins/treesitter-context.lua
@@ -0,0 +1,4 @@
+return {
+  "nvim-treesitter-context",
+  opts = { mode = "cursor", max_lines = 1 },
+}
