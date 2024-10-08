0c18179672053906682279ed186f450fee6c86ea
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2024-10-08 10:36:36 +0200

    [nvim] Fix configuration error for buffermanager an switch back to main version

--- nvim/lua/custom/plugins/buffermanager.lua
+++ nvim/lua/custom/plugins/buffermanager.lua
@@ -1,19 +1,24 @@
 return {
-  -- "j-morano/buffer_manager.nvim",
-  "kschweiger/buffer_manager.nvim",
+  "j-morano/buffer_manager.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local buffer_manager = require("buffer_manager")
-    local ui = require("buffer_manager.ui")
-    buffer_manager.setup({
-      short_file_names = true,
-      short_term_names = true,
-      width = 80,
-      height = 20,
-    })
-    vim.keymap.set("n", "<leader>bl", function()
-      ui.toggle_quick_menu()
-    end, { desc = "List buffers" })
+  opts = {
+    short_file_names = true,
+    short_term_names = true,
+    width = 80,
+    height = 20,
+  },
+  keys = {
+    {
+      "<leader>bl",
+      function()
+        require("buffer_manager.ui").toggle_quick_menu()
+      end,
+      desc = "List Buffers",
+      -- mode = { "n" },
+    },
+  },
+  config = function(_, opts)
+    require("buffer_manager").setup(opts)
     vim.api.nvim_set_hl(0, "BufferManagerModified", { fg = "#f5bde6", bg = "#494d64" })
   end,
 }
